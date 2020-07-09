7d5c92ceb63c6155c3aa0565b26334b87047397b
Shuhei Hiya <syuhei176@gmail.com>
2020-07-09 12:10:32 +0900

    separate custom types

--- packages/plasma-aggregator/src/index.ts
+++ packages/plasma-aggregator/src/index.ts
@@ -11,6 +11,7 @@ import {
 import Keyring from '@polkadot/keyring'
 import { ApiPromise, WsProvider } from '@polkadot/api'
 import { stringToU8a } from '@polkadot/util'
+import customTypes from './customTypes'
 import { setupContext } from '@cryptoeconomicslab/context'
 setupContext({
   coder: PolcadotCoder
@@ -41,61 +42,7 @@ const instantiate = async (): Promise<Aggregator> => {
   )
   const apiPromise = await ApiPromise.create({
     provider,
-    types: {
-      Decision: {
-        _enum: ['Undecided', 'True', 'False']
-      },
-      ChallengeGameOf: {
-        property: 'Property',
-        challenges: 'Vec<Hash>',
-        decision: 'Decision',
-        created_block: 'BlockNumber'
-      },
-      Checkpoint: {
-        state_update: 'StateUpdate',
-        sub_range: 'Range'
-      },
-      OfferOf: {},
-      Parameters: {
-        can_be_nominated: 'bool',
-        option_expired: 'u128',
-        option_p: 'u128'
-      },
-      PredicateContractOf: {
-        predicate_hash: 'CodeHash',
-        inputs: 'Vec<u8>'
-      },
-      PredicateHash: 'Hash',
-      PrefabOvmModule: {
-        schedule_version: 'u32',
-        code: 'Vec<u8>'
-      },
-      Property: {
-        predicate_address: 'AccountId',
-        inputs: 'Vec<Vec<u8>>'
-      },
-      PropertyOf: {
-        predicate_address: 'AccountId',
-        inputs: 'Vec<Vec<u8>>'
-      },
-      Range: {
-        start: 'u128',
-        end: 'u128'
-      },
-      RangeOf: {
-        start: 'u128',
-        end: 'u128'
-      },
-      StateObject: {
-        predicate: 'AccountId',
-        data: 'Vec<u8>'
-      },
-      StateUpdate: {
-        range: 'Range',
-        state_object: 'StateObject',
-        plasma_block_number: 'BlockNumber'
-      }
-    }
+    types: customTypes
   })
 
   const wallet = new SubstrateWallet(keyring)
